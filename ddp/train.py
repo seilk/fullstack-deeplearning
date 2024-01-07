65abb1412f634e9cdd4a72b2ec2b407cdaacac98
seilk <88271769+seilk@users.noreply.github.com>
2024-01-07 22:35:10 +0900

    update train.py

--- ddp/train.py
+++ ddp/train.py
@@ -1,6 +1,8 @@
 from attrs import define, field
 import torch
 import argparse
+import torch.nn as nn
+from collections import OrderedDict
 
 @define
 class Trainer:
@@ -18,10 +20,10 @@ class Trainer:
         Implement the training logic here.
         """
         for epoch in range(self.epochs):
-            self.train_epoch()
+            self.train_epoch(self.model, self.train_loader, self.optimizer)
             self.validate_epoch()
 
-    def train_epoch(self):
+    def train_epoch(self, model, train_loader, optimizer):
         """
         Implement the training logic for one epoch.
         """
@@ -29,6 +31,7 @@ class Trainer:
         for batch in self.train_loader:
             # Training logic for each batch
             ...
+            self.save_model_state_dict(model, 'model_state.pth')
 
     def validate_epoch(self):
         """
@@ -40,6 +43,22 @@ class Trainer:
                 # Validation logic for each batch
                 ...
 
+    def save_model_state_dict(self, model, filename):
+        try:
+            # Try to access the underlying model in DDP
+            state_dict = model.module.state_dict()
+        except AttributeError:
+            # If model is not wrapped in DDP, save normally
+            state_dict = model.state_dict()
+
+        # Optionally, you can remove 'module.' prefix from state_dict keys
+        clean_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            name = k[7:] if k.startswith('module.') else k
+            clean_state_dict[name] = v
+
+        torch.save(clean_state_dict, filename)
+    
 # Example usage
 # trainer = Trainer(model, train_loader, val_loader, optimizer)
 # trainer()
